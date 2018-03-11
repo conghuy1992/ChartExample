651b37fe10bab63cfabe7ca730980f995526f3ac
conghuy1992 <huynhconghuy92@gmail.com>
2018-03-11 17:47:18 +0700

    --

--- app/src/main/java/com/example/android/demochart/MainActivity.java
+++ app/src/main/java/com/example/android/demochart/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.demochart;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -16,6 +18,12 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.BubbleData;
+import com.github.mikephil.charting.data.BubbleDataSet;
+import com.github.mikephil.charting.data.BubbleEntry;
+import com.github.mikephil.charting.data.CandleData;
+import com.github.mikephil.charting.data.CandleDataSet;
+import com.github.mikephil.charting.data.CandleEntry;
 import com.github.mikephil.charting.data.CombinedData;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -33,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
 
         CombinedChart combinedChart = (CombinedChart) findViewById(R.id.chart1);
 
+
+//        Bitmap starBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+//        combinedChart.setRenderer(new ImageBarChartRenderer(combinedChart, combinedChart.getAnimator(), combinedChart.getViewPortHandler(), starBitmap));
+
         combinedChart.setBackgroundColor(Color.WHITE);
         combinedChart.setDrawGridBackground(false);
         combinedChart.setDrawBarShadow(false);
@@ -45,9 +57,13 @@ public class MainActivity extends AppCompatActivity {
         combinedChart.getAxisRight().setDrawAxisLine(false);
         combinedChart.getAxisRight().setDrawLabels(false);
 
+//        mChart.setDrawOrder(new DrawOrder[]{
+//                DrawOrder.BAR, DrawOrder.BUBBLE, DrawOrder.CANDLE, DrawOrder.LINE, DrawOrder.SCATTER
+//        });
+
         // draw bars behind lines
         combinedChart.setDrawOrder(new CombinedChart.DrawOrder[]{
-                CombinedChart.DrawOrder.BAR, CombinedChart.DrawOrder.LINE
+                CombinedChart.DrawOrder.BAR, CombinedChart.DrawOrder.LINE, CombinedChart.DrawOrder.BUBBLE
         });
 
         YAxis rightAxis = combinedChart.getAxisRight();
@@ -64,9 +80,15 @@ public class MainActivity extends AppCompatActivity {
         CombinedData data = new CombinedData(getXAxisValues());
         data.setData(generateBarData());
         data.setData(generateLineData());
+        data.setData(generateBubbleData());
+//        data.setData(generateScatterData());
+//        data.setData(generateCandleData());
+
 
         combinedChart.setData(data);
         combinedChart.invalidate();
+
+
     }
 
     private float getRandom(float range, float startsfrom) {
@@ -77,32 +99,95 @@ public class MainActivity extends AppCompatActivity {
             "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
     };
 
+    protected CandleData generateCandleData() {
+
+//        ArrayList<CandleEntry> entries = new ArrayList<CandleEntry>();
+//        for (int index = 0; index < mMonths.length; index++)
+//            entries.add(new CandleEntry(index, 90, 70, 85, 75f));
+//        CandleDataSet set = new CandleDataSet(entries, "Candle DataSet");
+//        set.setDecreasingColor(Color.rgb(111, 111, 111));
+//        set.setShadowColor(Color.DKGRAY);
+//        set.setValueTextSize(10f);
+//        set.setDrawValues(false);
+//        CandleData d = new CandleData(getXAxisValues(), set);
+//        d.addDataSet(set);
+//
+//        return d;
+
+        ArrayList<CandleEntry> entries = new ArrayList<CandleEntry>();
+
+        for (int index = 0; index < mMonths.length; index++)
+            entries.add(new CandleEntry(index, 90, 70, 85, 75f));
+
+        CandleDataSet set = new CandleDataSet(entries, "Candle DataSet");
+        set.setDecreasingColor(Color.rgb(142, 150, 175));
+        set.setShadowColor(Color.DKGRAY);
+        set.setBodySpace(0.3f);
+        set.setValueTextSize(10f);
+        set.setDrawValues(false);
+
+        CandleData d = new CandleData(getXAxisValues(), set);
+
+        return d;
+    }
+
+    protected BubbleData generateBubbleData() {
+
+//        BubbleData bd = new BubbleData();
+//
+//        ArrayList<BubbleEntry> entries = new ArrayList<BubbleEntry>();
+//
+//        for (int index = 0; index < mMonths.length; index++) {
+//            float y = getRandom(10, 105);
+//            float size = getRandom(100, 105);
+//            entries.add(new BubbleEntry(index, y, size));
+//        }
+//
+//        BubbleDataSet set = new BubbleDataSet(entries, "Bubble DataSet");
+//        set.setColors(ColorTemplate.VORDIPLOM_COLORS);
+//        set.setValueTextSize(10f);
+//        set.setValueTextColor(Color.WHITE);
+//        set.setHighlightCircleWidth(1.5f);
+//        set.setDrawValues(true);
+//        bd.addDataSet(set);
+//
+//        return bd;
+
+        ArrayList<BubbleEntry> entries = new ArrayList<>();
+
+        for (int index = 0; index < mMonths.length; index++) {
+            float y = getRandom(50, 30);
+            float size = getRandom(100, 105);
+            entries.add(new BubbleEntry(index, y, size));
+        }
+        BubbleDataSet set = new BubbleDataSet(entries, "BubbleDataSet");
+        set.setColor(Color.rgb(240, 238, 70));
+
+//        set.setLineWidth(2.5f);
+//        set.setCircleColor(Color.rgb(240, 238, 70));
+//        set.setFillColor(Color.rgb(240, 238, 70));
+        set.setDrawValues(true);
+        set.setValueTextSize(10f);
+        set.setValueTextColor(Color.rgb(240, 238, 70));
+        BubbleData d = new BubbleData(getXAxisValues(), set);
+        return d;
+    }
+
     private LineData generateLineData() {
-
-
         ArrayList<Entry> entries = new ArrayList<Entry>();
 
         for (int index = 0; index < mMonths.length; index++)
             entries.add(new Entry(getRandom(15, 5), index));
-
         LineDataSet set = new LineDataSet(entries, "Line DataSet");
         set.setColor(Color.rgb(240, 238, 70));
 
         set.setLineWidth(2.5f);
         set.setCircleColor(Color.rgb(240, 238, 70));
-//        set.setCircleRadius(5f);
         set.setFillColor(Color.rgb(240, 238, 70));
-//        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
         set.setDrawValues(true);
         set.setValueTextSize(10f);
         set.setValueTextColor(Color.rgb(240, 238, 70));
-//
-//        set.setAxisDependency(YAxis.AxisDependency.LEFT);
-//        d.addDataSet(set);
-
         LineData d = new LineData(getXAxisValues(), set);
-
-
         return d;
     }
 
